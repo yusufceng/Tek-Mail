bf664d76371674e49ebc10ba5ed754a643b1b484
yusufceng <yusufceng95@gmail.com>
2020-08-03 19:27:28 +0300

    fix some problems and daily commit

--- serverfiles/arr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-array=$(ls)
-for value in "${array[@]}"
-do
-     echo $value
-done
-echo "done"
-echo ${array[1]}
--- serverfiles/test.py
+++
@@ -1,8 +0,0 @@
-from firebase import firebase
-import sys
-
-
-firebase = firebase.FirebaseApplication("",None)
-filename=sys.argv[1]
-print(filename)
-#f=open("")
